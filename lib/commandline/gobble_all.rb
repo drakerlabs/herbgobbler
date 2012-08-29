8865efd803a0ef8fe8d7c0e0988be1b2c74cce60
alaroche <alaroche22@gmail.com>
2012-08-29 10:31:07 -0400

    Updated Herb Extractor to not clobber existing translation files.
    Allows for per-directory translation.

--- lib/commandline/gobble_all.rb
+++ lib/commandline/gobble_all.rb
@@ -47,27 +47,32 @@ class GobbleAll
     
     rails_view_directory = "#{@rails_root}/app/views"
     
-    rails_translation_store = RailsTranslationStore.load_from_file( full_yml_file_path )
-    text_extractor = RailsTextExtractor.new( rails_translation_store )
-    
-    Dir["#{rails_view_directory}/**/*html.erb" ].each do |full_erb_file_path|
-      directory_path = full_erb_file_path.match(/views\/(.*)\//).to_s.split('/')
+    Dir.glob("#{rails_view_directory}/**/*.html.erb").group_by { |x| File.dirname(x) }.each do |directory, files|
+      directory_path = File.dirname(directory)
+      yml_name = File.basename(directory)+".yml"
 
-      yml_name = directory_path.last+".yml"
-      directory_path.pop
+      full_yml_file_path = "#{@rails_root}/config/locales/#{language_code}/#{directory_path}/#{yml_name}"
 
-      full_yml_file_path = "#{@rails_root}/config/locales/#{language_code}/#{directory_path.join('/')}/#{yml_name}"
-      
-      erb_file = full_erb_file_path.gsub( @rails_root, '' )
-      rails_translation_store.start_new_context( convert_path_to_key_path( erb_file.to_s ) )
-      erb_file = ErbFile.load( full_erb_file_path )
-      erb_file.extract_text( text_extractor )
+      rails_translation_store = if File.exists?(full_yml_file_path)
+        RailsTranslationStore.load_from_file( full_yml_file_path )
+      else
+        RailsTranslationStore.new
+      end
 
-      File.open(full_erb_file_path, 'w') {|f| f.write(erb_file.to_s) }
-      puts "Wrote #{full_erb_file_path}"
+      text_extractor = RailsTextExtractor.new( rails_translation_store )
 
-      File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
-      puts "Wrote #{full_yml_file_path}"    
+      files.each do |full_erb_file_path|
+        erb_file = full_erb_file_path.gsub( @rails_root, '' )
+        rails_translation_store.start_new_context( convert_path_to_key_path( erb_file.to_s ) )
+        erb_file = ErbFile.load( full_erb_file_path )
+        erb_file.extract_text( text_extractor )
+
+        File.open(full_erb_file_path, 'w') {|f| f.write(erb_file.to_s) }
+        puts "Wrote #{full_erb_file_path}"
+
+        File.open(full_yml_file_path, 'w') {|f| f.write(rails_translation_store.serialize) }
+        puts "Wrote #{full_yml_file_path}"
+      end
     end
   end
   
